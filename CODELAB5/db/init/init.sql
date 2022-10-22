d96e54f30d0e5fd71b65e9203cbe115b53e03b8e
KomalSrivas <114260948+KomalSrivas@users.noreply.github.com>
2022-10-21 18:13:36 -0700

    Add files via upload

---
+++ CODELAB5/db/init/init.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE IF NOT EXISTS Meals (
+  foodName TEXT NOT NULL,
+  price REAL NOT NULL
+);
+
+INSERT INTO Meals(foodName, price) VALUES ('Chicken Salad',10.00);
+INSERT INTO Meals(foodName, price) VALUES ('Coffee',5.00);
+INSERT INTO Meals(foodName, price) VALUES ('Shakes',5.00);
+INSERT INTO Meals(foodName, price) VALUES ('Soups',10.00);
+INSERT INTO Meals(foodName, price) VALUES ('Egg curry',10.50);
+INSERT INTO Meals(foodName, price) VALUES ('Burrito',10.75);
+INSERT INTO Meals(foodName, price) VALUES ('Fried rice',12.00);
+INSERT INTO Meals(foodName, price) VALUES ('Noodles',108.00);
+INSERT INTO Meals(foodName, price) VALUES ('Pizza',28.00);
+INSERT INTO Meals(foodName, price) VALUES ('Burger',7.50);
+INSERT INTO Meals(foodName, price) VALUES ('Ramen',10.75);
+INSERT INTO Meals(foodName, price) VALUES ('Chicken Tacos',8.54);
+INSERT INTO Meals(foodName, price) VALUES ('Green Thai curry',1.56);
+INSERT INTO Meals(foodName, price) VALUES ('Padthai',8.50);
+INSERT INTO Meals(foodName, price) VALUES ('Popcorn chicken',7.50);
+INSERT INTO Meals(foodName, price) VALUES ('Boba',5.75);
